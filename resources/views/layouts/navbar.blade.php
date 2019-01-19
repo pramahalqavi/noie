9ff19b990efd8ef8fd7a4b6da28a40ea344b7e4b
pramahalqavi <pramahalqavi97@gmail.com>
2019-01-19 22:49:03 +0700

    add slideshow image on collections

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,7 +5,7 @@
         <title>NOIE</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Thasadith" rel="stylesheet">
-        <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link href="{{asset('css/default.css')}}" rel="stylesheet" type="text/css">
 
     </head>
@@ -17,29 +17,29 @@
         @else
         <div class="background" style="background-color: #eee">
         @endif
-            <div class="nav">
-                <div class="logo">
+            <div class="noie-nav">
+                <div class="noie-logo">
                     NOIE
                 </div>
-                <div class="navbar">
-                    <a class="navbutton" href="{{url('/')}}">HOME</a>
-                    <div class="dropdown">
-                        <a class="navbutton">COLLECTIONS </a>
-                        <div class="dropdown-content">  
-                            <div class="row">
-                                <div class="column">
+                <div class="noie-navbar">
+                    <a class="noie-navbutton" href="{{url('/')}}">HOME</a>
+                    <div class="noie-dropdown">
+                        <a class="noie-navbutton">COLLECTIONS </a>
+                        <div class="noie-dropdown-content">
+                            <div class="noie-row">
+                                <div class="noie-column">
                                     <h4>Category 1</h4>
                                     <a href="{{url('collections')}}">Link 1</a>
                                     <a href="">Link 2</a>
                                     <a href="">Link 3</a>
                                 </div>
-                                <div class="column">
+                                <div class="noie-column">
                                     <h4>Category 2</h4>
                                     <a href="#">Link 1</a>
                                     <a href="#">Link 2</a>
                                     <a href="#">Link 3</a>
                                 </div>
-                                <div class="column">
+                                <div class="noie-column">
                                     <h4>Category 3</h4>
                                     <a href="#">Link 1</a>
                                     <a href="#">Link 2</a>
@@ -49,16 +49,17 @@
                         </div>
                     </div>
 
-                    <a class="navbutton" href="{{url('about')}}">ABOUT</a>
-                    <a class="navbutton" href="{{url('payment-status')}}">PAYMENT STATUS</a>
+                    <a class="noie-navbutton" href="{{url('about')}}">ABOUT</a>
+                    <a class="noie-navbutton" href="{{url('payment-status')}}">PAYMENT STATUS</a>
                 </ul>
             </div>
         </div>
         <div class="content">
             @yield('content')
         </div>
-        <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        @yield('javascript')
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     </body>
 </html>
