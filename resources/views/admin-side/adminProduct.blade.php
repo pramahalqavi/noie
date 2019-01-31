390cda396e43229eaf85beae242822f98bea7cb2
Ali Rido <mohalirido@gmail.com>
2019-01-31 19:55:02 +0700

    Moduling file admin site

---
+++ resources/views/admin-side/adminProduct.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.admin-navbar')
+
+@section('content')
+
+<h1 style="text-align: center">PRODUCT MANAGEMENT</h1>
+<h1>Periode 1 <button class="add_field_button btn btn-success btn-md"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button></h1>
+<div class="row">
+    <div class="col-lg-1" style="text-align: center"><i class="fas fa-chevron-left" style="font-size: 6em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-1" style="text-align: center"><i class="fas fa-chevron-right" style="font-size: 6em"></i></div>
+</div><div class="row">
+    <div class="col-lg-1" style="text-align: center"></div>
+    <div class="col-lg-2" style="text-align: center">Produk A</div>
+    <div class="col-lg-2" style="text-align: center">Produk B</div>
+    <div class="col-lg-2" style="text-align: center">Produk C</div>
+    <div class="col-lg-2" style="text-align: center">Produk D</div>
+    <div class="col-lg-2" style="text-align: center">Produk E</div>
+    <div class="col-lg-1" style="text-align: center"></div>
+</div>
+<h1>Periode 2 <button class="add_field_button btn btn-success btn-md"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button></h1>
+<div class="row">
+    <div class="col-lg-1" style="text-align: center"><i class="fas fa-chevron-left" style="font-size: 6em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-1" style="text-align: center"><i class="fas fa-chevron-right" style="font-size: 6em"></i></div>
+</div><div class="row">
+    <div class="col-lg-1" style="text-align: center"></div>
+    <div class="col-lg-2" style="text-align: center">Produk A</div>
+    <div class="col-lg-2" style="text-align: center">Produk B</div>
+    <div class="col-lg-2" style="text-align: center">Produk C</div>
+    <div class="col-lg-2" style="text-align: center">Produk D</div>
+    <div class="col-lg-2" style="text-align: center">Produk E</div>
+    <div class="col-lg-1" style="text-align: center"></div>
+</div>
+<h1>Periode 3 <button class="add_field_button btn btn-success btn-md"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button></h1>
+<div class="row">
+    <div class="col-lg-1" style="text-align: center"><i class="fas fa-chevron-left" style="font-size: 6em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-1" style="text-align: center"><i class="fas fa-chevron-right" style="font-size: 6em"></i></div>
+</div><div class="row">
+    <div class="col-lg-1" style="text-align: center"></div>
+    <div class="col-lg-2" style="text-align: center">Produk A</div>
+    <div class="col-lg-2" style="text-align: center">Produk B</div>
+    <div class="col-lg-2" style="text-align: center">Produk C</div>
+    <div class="col-lg-2" style="text-align: center">Produk D</div>
+    <div class="col-lg-2" style="text-align: center">Produk E</div>
+    <div class="col-lg-1" style="text-align: center"></div>
+</div>
+<h1>Periode 4 <button class="add_field_button btn btn-success btn-md"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button></h1>
+<div class="row">
+    <div class="col-lg-1" style="text-align: center"><i class="fas fa-chevron-left" style="font-size: 6em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-2" style="text-align: center"><i class="fas fa-image" style="font-size: 8em"></i></div>
+    <div class="col-lg-1" style="text-align: center"><i class="fas fa-chevron-right" style="font-size: 6em"></i></div>
+</div><div class="row">
+    <div class="col-lg-1" style="text-align: center"></div>
+    <div class="col-lg-2" style="text-align: center">Produk A</div>
+    <div class="col-lg-2" style="text-align: center">Produk B</div>
+    <div class="col-lg-2" style="text-align: center">Produk C</div>
+    <div class="col-lg-2" style="text-align: center">Produk D</div>
+    <div class="col-lg-2" style="text-align: center">Produk E</div>
+    <div class="col-lg-1" style="text-align: center"></div>
+</div>
+<br>
+<button class="add_field_button btn btn-success btn-md"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Add Periode</button>
+                    
+@endsection
