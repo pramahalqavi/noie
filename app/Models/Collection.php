b4f99a66a9fb3022f93f7dc488e4d3a91ceaef3f
Ali Rido <mohalirido@gmail.com>
2019-02-14 21:40:11 +0700

    Add collection model

---
+++ app/Models/Collection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Collection extends Model
+{
+    //
+
+    public $timestamps = false; // Because not using created_at and updated_at
+}
