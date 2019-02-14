085edc15b953b79c113a09bb31a3ffb481137040
Ali Rido <mohalirido@gmail.com>
2019-02-14 18:27:58 +0700

    Add yield title

--- resources/views/layouts/admin-navbar.blade.php
+++ resources/views/layouts/admin-navbar.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 
+    <title>@yield('title')</title>
+
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
