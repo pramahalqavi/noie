5a6c33b5e71505008fd039a05317ef1f11392f61
Ali Rido <mohalirido@gmail.com>
2019-02-15 08:02:50 +0700

    Update frontend to be more dynamic and beautiful

--- resources/views/admin-side/adminProduct.blade.php
+++ resources/views/admin-side/adminProduct.blade.php
@@ -9,7 +9,14 @@
 <hr style="width: .2%">
 
 @foreach ($years as $year)
-    <hr class="right-line"><h2><i class="fas fa-angle-right"></i> {{ $year->year }}</h2>
+    <hr class="right-line"><h2 onclick="showCollection('{{$year->year}}')"><i id="arrow{{$year->year}}" class="fas fa-angle-right"></i> {{ $year->year }}</h2>
+    <ol id="show{{$year->year}}" class="collection-list" style="display: none">
+        @foreach ($collections as $collection)
+            @if ($collection->year == $year->year)
+            <li>{{ $collection->name }}</li>
+            @endif
+        @endforeach
+    </ol>
 @endforeach
 
 <!-- <h1>Periode 1 <button class="add_field_button btn btn-success btn-md"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button></h1>
@@ -85,6 +92,6 @@
     <div class="col-lg-1" style="text-align: center"></div>
 </div> -->
 <br>
-<button class="add_field_button btn btn-success btn-md"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Add Periode</button>
+<button class="add_field_button btn btn-success btn-md"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add Periode</button>
                     
 @endsection
