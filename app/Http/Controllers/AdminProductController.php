b502ced37a191afff342bbda5505bb1a8a0522db
Ali Rido <mohalirido@gmail.com>
2019-02-15 08:01:10 +0700

    Add addtional query for product index function

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -13,9 +13,11 @@ class AdminProductController extends Controller
 	public function index() {
     	
     	$years = Collection::distinct()->orderBy('year', 'desc')->get(['year']);
+
+    	$collections = Collection::orderBy('year', 'desc')->get();
     	// dd($collections);
 
-		return view('admin-side/adminProduct', ['years' => $years]);
+		return view('admin-side/adminProduct', ['years' => $years, 'collections' => $collections]);
 	}
 
 
