5476e3e949bced379c3edfc6b80464008f944acb
Ali Rido <mohalirido@gmail.com>
2019-02-16 12:35:29 +0700

    Add addYear function

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -20,5 +20,28 @@ class AdminProductController extends Controller
 		return view('admin-side/adminProduct', ['years' => $years, 'collections' => $collections]);
 	}
 
+	public function addYear(Request $request) {
+		$collection1 = new Collection;
+		$collection1->year = $request->year;
+		$collection1->name = $request->collection1;
+		$collection1->save();
+
+		$collection2 = new Collection;
+		$collection2->year = $request->year;
+		$collection2->name = $request->collection2;
+		$collection2->save();
+
+		$collection3 = new Collection;
+		$collection3->year = $request->year;
+		$collection3->name = $request->collection3;
+		$collection3->save();
+
+		$collection4 = new Collection;
+		$collection4->year = $request->year;
+		$collection4->name = $request->collection4;
+		$collection4->save();
+
+		return redirect('admin/product');
+	}
 
 }
