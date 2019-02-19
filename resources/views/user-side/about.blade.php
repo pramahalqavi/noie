c2cb260de485b05595d3ea9fc75e26e40c0a18b2
pramahalqavi <pramahalqavi97@gmail.com>
2019-02-19 14:58:04 +0700

    change about and product layout

--- resources/views/user-side/about.blade.php
+++ resources/views/user-side/about.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.navbar')
 
 @section('content')
-<div class="rowblock-2">
-  <div class="columnblock-2" style="background-color: rgba(55,55,55,0.4);">
-    <h2>ABOUT</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p><img src="{{asset('images/fb-icon.png')}}" class="socmed-icon"/> Facebook </p>
-    <p><img src="{{asset('images/twitter-icon.png')}}" class="socmed-icon" /> Twitter </p>
-    <p><img src="{{asset('images/ig-icon.png')}}" class="socmed-icon" /> Instagram</p>
+<div class="container-about container">
+  <div class="row row-about" style="background-color: rgba(55,55,55,0.6);">
+    <div class="col-md">
+      <h2>ABOUT</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p><img src="{{asset('images/fb-icon.png')}}" class="socmed-icon"/> Facebook </p>
+      <p><img src="{{asset('images/twitter-icon.png')}}" class="socmed-icon" /> Twitter </p>
+      <p><img src="{{asset('images/ig-icon.png')}}" class="socmed-icon" /> Instagram</p>
+    </div>
+    <div class="col"> </div>
   </div>
 </div>
 @endsection
--- resources/views/user-side/products.blade.php
+++ resources/views/user-side/products.blade.php
@@ -1,46 +1,48 @@
 @extends('layouts.navbar')
 
 @section('content')
-<div class="rowblock-3">
-    <div class="columnblock-3">
-        <div class="img-slides">
-            <img class="product-image" src="{{asset('images/shoes1.jpg')}}" style="width:100%">
+<div class="container-fluid container-product">
+    <div class="row">
+        <div class="col-md-4 img-col">
+            <div class="img-slides product-img-l">
+                <img class="product-image" src="{{asset('images/shoes1.jpg')}}" style="width:100%">
+            </div>
         </div>
-    </div>
-    <div class="columnblock-3">
-        <h2 class="product-collection" style="font-size: 26px"> Collection 1 </h3>
-        <h2 class="product-name" style="font-size: 26px"> Name 1 </h2>
-        <hr style="width: 70%; background-color: #888;">
-        <p class="price" style="font-size: 26px"> $ 1,000 </p>
-        <p class="material" style="font-size: 22px"> Plastic Material </p>
-            <p style="display: inline-block; font-size: 18px; margin-right: 25px;"> SIZE: </p>
-            <label class="noie-container"> S 
-                <input type="radio" name="size" id="radio1" value="S"/>
-                <span class="noie-checkmark"></span>
-            </label>
-            <label class="noie-container"> M
-                <input type="radio" name="size" id="radio2" value="M" checked="checked"/>
-                <span class="noie-checkmark"></span>
-            </label>
-            <label class="noie-container"> L 
-                <input type="radio" name="size" id="radio3" value="L"/>
-                <span class="noie-checkmark"></span>
-            </label>
-            <label class="noie-container"> XL 
-                <input type="radio" name="size" id="radio4" value="XL"/> 
-                <span class="noie-checkmark"></span>
-            </label>
-        <br>
-        <button href="{{url('collections/order')}}}"> BUY NOW </button>
-        <div style="text-align:center; height: auto; width: auto">
-            <span class="dot" onclick=""></span> 
-            <span class="dot" onclick=""></span> 
-            <span class="dot" onclick=""></span> 
+        <div class="col-md-4">
+            <h2 class="product-collection" style="font-size: 26px"> Collection 1 </h3>
+            <h2 class="product-name" style="font-size: 26px"> Name 1 </h2>
+            <hr style="width: 70%; background-color: #888;">
+            <p class="price" style="font-size: 26px"> $ 1,000 </p>
+            <p class="material" style="font-size: 22px"> Plastic Material </p>
+                <p style="display: inline-block; font-size: 18px; margin-right: 25px;"> SIZE: </p>
+                <label class="noie-container"> S 
+                    <input type="radio" name="size" id="radio1" value="S"/>
+                    <span class="noie-checkmark"></span>
+                </label>
+                <label class="noie-container"> M
+                    <input type="radio" name="size" id="radio2" value="M" checked="checked"/>
+                    <span class="noie-checkmark"></span>
+                </label>
+                <label class="noie-container"> L 
+                    <input type="radio" name="size" id="radio3" value="L"/>
+                    <span class="noie-checkmark"></span>
+                </label>
+                <label class="noie-container"> XL 
+                    <input type="radio" name="size" id="radio4" value="XL"/> 
+                    <span class="noie-checkmark"></span>
+                </label>
+            <br>
+            <button href="{{url('collections/order')}}}"> BUY NOW </button>
+            <div style="text-align:center; height: auto; width: auto">
+                <span class="dot" onclick=""></span> 
+                <span class="dot" onclick=""></span> 
+                <span class="dot" onclick=""></span> 
+            </div>
         </div>
-    </div>
-    <div class="columnblock-3">
-        <div class="img-slides">
-            <img class="product-image" src="{{asset('images/shoes2.jpg')}}" style="width:100%">
+        <div class="col-md-4 img-col">
+            <div class="img-slides product-img-r">
+                <img class="product-image" src="{{asset('images/shoes2.jpg')}}" style="width:100%">
+            </div>
         </div>
     </div>
 </div>
