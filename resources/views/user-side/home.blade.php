b0c1c593519a225a410daa11aa5f534886a171c1
pramahalqavi <pramahalqavi97@gmail.com>
2019-02-24 23:28:49 +0700

    add success message pop up after submit order

--- resources/views/user-side/home.blade.php
+++ resources/views/user-side/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     @if(Session::has('message'))
-    <!-- <div id="note">
-        You smell good. <a id="close">[close]</a>
-    </div> -->
+    <div id="note">
+        Order successfully submitted. Check your email for details.
+    </div>
     @endif
     <h1 style="font-size: 50px; color: white;">  </h1>
 @endsection
