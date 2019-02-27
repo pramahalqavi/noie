b42c1c6b96dd15496c8ce9a7d11e26b4c4416493
Ali Rido <mohalirido@gmail.com>
2019-02-27 22:16:02 +0700

    Update db

--- noie.sql
+++ noie.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 27, 2019 at 08:21 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.1.13
+-- Host: localhost
+-- Generation Time: Feb 27, 2019 at 04:15 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -91,7 +91,7 @@ CREATE TABLE `products` (
   `image2` varchar(255) NOT NULL DEFAULT 'storage/product/product-img.jpg',
   `thumbnail1` varchar(255) NOT NULL DEFAULT 'storage/product/thumbnail/product-img.jpg',
   `thumbnail2` varchar(255) NOT NULL DEFAULT 'storage/product/thumbnail/product-img.jpg',
-  `material` varchar(255) NOT NULL,
+  `material` varchar(255) NOT NULL DEFAULT '-',
   `size` varchar(255) DEFAULT '-',
   `price` int(100) NOT NULL,
   `collection_id` int(11) NOT NULL
@@ -108,8 +108,7 @@ INSERT INTO `products` (`product_id`, `name`, `image1`, `image2`, `thumbnail1`, 
 (5, 'Typing', 'storage/product/5-1.png', 'storage/product/5-2.png', 'storage/product/thumbnail/5-1.png', 'storage/product/thumbnail/5-2.png', 'Keyboard', 'One size', 200000, 9),
 (6, 'Toyota New Alphard', 'storage/product/6-1.png', 'storage/product/6-2.png', 'storage/product/thumbnail/6-1.png', 'storage/product/thumbnail/6-2.png', 'V6 Machine', 'Aa Bb Cc', 959000000, 9),
 (7, 'Jaket Parka', 'storage/product/product-img.jpg', 'storage/product/product-img.jpg', 'storage/product/thumbnail/product-img.jpg', 'storage/product/thumbnail/product-img.jpg', 'Primeknit', 'S M L', 500000, 13),
-(11, 'Cobain', 'storage/product/11-1.jpg', 'storage/product/11-2.png', 'storage/product/thumbnail/11-1.jpg', 'storage/product/thumbnail/11-2.png', 'Primeknit', '40 41 42 43 44 45', 500000, 9),
-(12, 'Wah', 'storage/product/12-1.png', 'storage/product/12-2.png', 'storage/product/thumbnail/12-1.png', 'storage/product/thumbnail/12-2.png', 'Waw', '40 41 42 43 44', 400000, 9);
+(13, 'Honda Jazz', 'storage/product/13-1.jpg', 'storage/product/13-2.jpg', 'storage/product/thumbnail/13-1.jpg', 'storage/product/thumbnail/13-2.jpg', 'Real steel', '-', 279000000, 9);
 
 -- --------------------------------------------------------
 
@@ -246,7 +245,7 @@ ALTER TABLE `collections`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
